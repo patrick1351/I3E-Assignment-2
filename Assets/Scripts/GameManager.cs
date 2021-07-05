ec06b3c47892ad2e10980e7bc7c5dfa9c943388f
Russchew <S10207891@connect.np.edu.sg>
2021-07-05 23:47:00 +0800

    Commit
    Commit 2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public Texture2D crosshairRed;
+    public Texture2D crosshairGreen;
+    private Texture2D crosshair;
+
+    public bool lookAtItem;
+
+    public int magicStone;
+
+    public TextMeshProUGUI questUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        if (lookAtItem)
+        {
+            crosshair = crosshairGreen;
+        }
+        else
+        {
+            crosshair = crosshairRed;
+        }
+
+        Vector2 cursorLocation = new Vector2(crosshair.width / 2, crosshair.height / 2);
+        Cursor.SetCursor(crosshair, cursorLocation, CursorMode.Auto);
+
+        questUI.SetText("Quest\n" +
+                        "Magic Stone: {0}", magicStone);
+    }
+}
