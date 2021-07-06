0b004164128779c13054dab0b4cfe2db9193e0d8
patrick1351 <s10194041@connect.np.edu.sg>
2021-07-06 20:43:31 +0800

    Got started on quest giver code

---
+++ Assets/Scripts/QuestGiver.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuestGiver : MonoBehaviour
+{
+    private int currentStage;
+
+    public int[] stageOneQuest = new int[3] { 1, 3, 3 };
+
+    public TextMeshProUGUI dialogueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
