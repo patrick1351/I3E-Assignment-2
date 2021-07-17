cbcee93fedc2d07c9be732a4119a7f817e0f9f56
patrick1351 <s10194041@connect.np.edu.sg>
2021-07-17 13:34:41 +0800

    Added fading between location and changed some coding

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    public GameObject player;
+    public Animator animator;
+    private Vector3 areaCordinate;
+    public void FadeOut()
+    {
+        animator.SetTrigger("Fade");
+    }
+
+    public void FadeIn()
+    {
+        player.transform.position = areaCordinate;
+        animator.SetTrigger("Fade");
+    }
+
+    public void LocationCordinate(float x, float y, float z)
+    {
+        areaCordinate = new Vector3(x, y, z);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,17 +12,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float rotationSpeed;
 
+    
+
+    /// <summary>
+    /// The distance of the raycast length
+    /// </summary>
     [SerializeField]
     private float interectionDistance;
 
-    private int currentStage;
 
     /// The camera attached to the player model should be dragged in from Inspector.
     public Camera playerCamera;
+    public GameObject cameraParent;
     public Rigidbody rigibody;
     public Vector3 velocity;
-
-    private bool jumping = false;
 
     private string currentState;
     private string nextState;
@@ -31,10 +34,20 @@ public class PlayerController : MonoBehaviour
     public Vector3 cameraRotation;
 
     public GameManager gameManagerScript;
+    public Fade fade;
+
+    /// <summary>
+    /// Check that the player is on the ground
+    /// </summary>
+    public bool onGround;
+    public float jumpForce;
+    private int numOfJump;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        numOfJump = 1;
         nextState = "Idle";
 
         playerRotation = Vector3.zero;
@@ -54,96 +67,37 @@ public class PlayerController : MonoBehaviour
 
         CheckRotation();
 
-        //Set the layermask to the quest
-        //Only item with the quest will be affected 
-        int layerMaskQuest = 1 << LayerMask.NameToLayer("Quest");
-        int layerMaskPortal = 1 << LayerMask.NameToLayer("Portal");
-        int layerMaskTop = 1 << LayerMask.NameToLayer("QuestTop");
-
-        //Draw line will show green if hit and red if no hit
-        RaycastHit hitInfo;
-        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hitInfo, interectionDistance, layerMaskQuest))
-        {
-            Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + playerCamera.transform.forward * interectionDistance, Color.green);
-            gameManagerScript.lookingAtItem = true;
-
-            if (Input.GetKeyDown("e"))
-            {
-                if (hitInfo.collider.gameObject.name == "Quest Giver")
-                {
-                    hitInfo.collider.gameObject.GetComponent<QuestGiver>().CheckStage();
-                }
-                
-                //Checking the object is correct
-                //Will change UI to reflect collection
-                if (hitInfo.collider.gameObject.name == "Magic Stone")
-                {
-                    ++gameManagerScript.magicStone;
-                    Debug.Log("Collecting magic stone");
-                }
-                else if (hitInfo.collider.gameObject.name == "Water")
-                {
-                    ++gameManagerScript.waterBottle;
-                    Debug.Log("Collecting water");
-                }
-                else if (hitInfo.collider.gameObject.name == "Flower")
-                {
-                    ++gameManagerScript.flower;
-                    Debug.Log("Collecting flower");
-                }
-            }
-        }
-        else if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hitInfo, interectionDistance, layerMaskPortal))
-        {
-            Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + playerCamera.transform.forward * interectionDistance, Color.green);
-            gameManagerScript.lookingAtItem = true;
-
-            if (Input.GetKeyDown("e"))
-            {
-                Debug.Log("Entering Portal");
-            }
-        }
-        else if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hitInfo, interectionDistance, layerMaskTop))
-        {
-            Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + playerCamera.transform.forward * interectionDistance, Color.green);
-            gameManagerScript.lookingAtItem = true;
-
-            if (Input.GetKeyDown("e"))
-            {
-                Debug.Log("Combining");
-                gameManagerScript.questTopDone = true;
-            }
-        }
-        else
-        {
-            gameManagerScript.lookingAtItem = false;
-            Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + playerCamera.transform.forward * interectionDistance, Color.red);
-        }
+        Raycasting();
 
 
         //Teleports player back to the platform if they fall off the island
-        if(this.transform.position.y < -20)
+        if (this.transform.position.y < -20)
         {
             Debug.Log("Falling into the void");
             this.transform.position = new Vector3(4, 0, 0);
         }
 
-        jumping = Input.GetButtonDown("Jump");
+        //Only when spacebar is pressed and the player is on the ground
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            if(numOfJump == 1)
+            {
+                numOfJump -= 1;
+                rigibody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                onGround = false;
+            }
+            
+        }
     }
 
     void FixedUpdate()
     {
-        if (jumping)
-        {
-            rigibody.velocity = new Vector3(0, 5, 0);
-        }
-
         velocity = rigibody.velocity;
 
         if (velocity.y < -8)
         {
-            Debug.Log("Dummy you falling ");
-            StartCoroutine(ScreenShake(.15f, .5f));
+            Debug.Log("Dummy you falling");
+            StartCoroutine(ScreenShake(.15f, .2f));
         }
     }
 
@@ -194,6 +148,100 @@ public class PlayerController : MonoBehaviour
         playerCamera.transform.rotation = Quaternion.Euler(cameraRotation);
     }
 
+
+    /// <summary>
+    /// Code for all the raycasting done with the player
+    /// </summary>
+    void Raycasting()
+    {
+        //Set the layermask to the quest
+        //Only item with the quest will be affected 
+        int layerMaskQuest = 1 << LayerMask.NameToLayer("Quest");
+        int layerMaskPortal = 1 << LayerMask.NameToLayer("Portal");
+        int layerMaskTop = 1 << LayerMask.NameToLayer("QuestTop");
+
+        //Draw line will show green if hit and red if no hit
+        RaycastHit hitInfo;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hitInfo, interectionDistance, layerMaskQuest))
+        {
+            Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + playerCamera.transform.forward * interectionDistance, Color.green);
+            gameManagerScript.lookingAtItem = true;
+
+            if (Input.GetKeyDown("e"))
+            {
+                if (hitInfo.collider.gameObject.name == "Quest Giver")
+                {
+                    hitInfo.collider.gameObject.GetComponent<QuestGiver>().CheckStage();
+                }
+
+                //Checking the object is correct
+                //Will change UI to reflect collection
+                if (hitInfo.collider.gameObject.tag == "MagicStone")
+                {
+                    ++gameManagerScript.magicStone;
+                    Debug.Log("Collecting magic stone");
+                    hitInfo.collider.gameObject.SetActive(false);
+                }
+                else if (hitInfo.collider.gameObject.tag == "Water")
+                {
+                    ++gameManagerScript.waterBottle;
+                    Debug.Log("Collecting water");
+                    hitInfo.collider.gameObject.SetActive(false);
+                }
+                else if (hitInfo.collider.gameObject.tag == "Flower")
+                {
+                    ++gameManagerScript.flower;
+                    Debug.Log("Collecting flower");
+                    hitInfo.collider.gameObject.SetActive(false);
+                }
+            }
+        }
+        else if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hitInfo, interectionDistance, layerMaskPortal))
+        {
+            Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + playerCamera.transform.forward * interectionDistance, Color.green);
+            gameManagerScript.lookingAtItem = true;
+
+            if (Input.GetKeyDown("e"))
+            {
+                if(hitInfo.collider.gameObject.tag == "AreaOne")
+                {
+                    fade.LocationCordinate(4, 0, -5);
+                } 
+                else if (hitInfo.collider.gameObject.tag == "AreaTwo")
+                {
+                    fade.LocationCordinate(-7, 0, -5);
+                }
+                else if (hitInfo.collider.gameObject.tag == "AreaThree")
+                {
+                    fade.LocationCordinate(0, 10, 0);
+                } 
+                else
+                {
+                    //Return back to hub
+                    fade.LocationCordinate(0, 10, 0);
+                }
+                Debug.Log("Entering Portal");
+                fade.FadeOut();
+            }
+        }
+        else if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hitInfo, interectionDistance, layerMaskTop))
+        {
+            Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + playerCamera.transform.forward * interectionDistance, Color.green);
+            gameManagerScript.lookingAtItem = true;
+
+            if (Input.GetKeyDown("e"))
+            {
+                Debug.Log("Combining");
+                gameManagerScript.questTopDone = true;
+            }
+        }
+        else
+        {
+            gameManagerScript.lookingAtItem = false;
+            Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + playerCamera.transform.forward * interectionDistance, Color.red);
+        }
+    }
+
     // Checks and handles movement of the player
     // Returns True if user input is detected and player is moved.
     private bool CheckMovement()
@@ -231,15 +279,27 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+        numOfJump = 1;
+    }
+    
+    /// <summary>
+    /// Shakes the screen based on the strength and duration
+    /// </summary>
+    /// <param name="duration">How long the shake last</param>
+    /// <param name="strength">How powerful the shake is</param>
+    /// <returns></returns>
     IEnumerator ScreenShake(float duration, float strength)
     {
-        Vector3 originalPos = playerCamera.transform.localPosition;
+        Vector3 originalPos = cameraParent.transform.localPosition;
 
         float timePass = 0.0f;
         while(timePass < duration)
         {
-            float x = Random.Range(-1f, 1f) * strength;
-            float y = Random.Range(-1f, 1f) * strength;
+            float x = Random.Range(-0.5f, 0.5f) * strength;
+            float y = Random.Range(-0.5f, 0.5f) * strength;
 
             playerCamera.transform.localPosition = new Vector3(x, y, originalPos.z);
 
@@ -248,6 +308,6 @@ public class PlayerController : MonoBehaviour
             yield return null;
         }
 
-        playerCamera.transform.localPosition = originalPos;
+        cameraParent.transform.localPosition = new Vector3(originalPos.x, originalPos.y, originalPos.z);
     }
 }
